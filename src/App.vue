0cf2a0ce230baaed403b93f7bb5ef68b83bf06e3
Kevin <kevin@nedellec.eu>
2022-06-03 11:26:29 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import TheHeader from "./components/TheHeader.vue";
+
+</script>
+
+<template>
+  <TheHeader />
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin-inline: auto;
+  padding: 2rem;
+}
+
+a,
+.accent {
+  text-decoration: none;
+  color: var(--color-accent);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  
+</script>
+
+<template>
+  <header>
+    <h1 class="logo">devfinder</h1>
+    <img src="@/assets/images/icon-sun.svg" alt="">
+  </header>
+</template>
+
+<style scoped lang="scss">
+  header {
+    display: flex;
+  }
+</style>
